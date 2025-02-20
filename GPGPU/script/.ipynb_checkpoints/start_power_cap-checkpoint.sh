0a8036e3b2d14d4a764edd57bca76ed1c569f14f
zzheng33 <zz695920754@gmail.com>
2025-02-20 03:59:30 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_power_cap-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_power_cap-checkpoint.sh
@@ -4,28 +4,13 @@
 # suite 1: ALTIS
 # suite 2: npb
 
-python3 exp_power_cap.py --suite 1 
-sleep 3
+# python3 exp_power_cap.py --suite 1 
+# sleep 3
+
 # ./power_util/set_uncore_freq.sh 2.2 2.2
 
 
 
-TARGET_DIR="../data/"
 
-# Find all .csv files under the target directory and make them writable
-sudo find "$TARGET_DIR" -type f -name "*.csv" -exec chown $(whoami):$(whoami) {} \;
+sudo chown -R cc:cc ../data/
 
-
-
-
-# TARGET_DIR="/home/cc/power/GPGPU/data/altis_power_cap_res/"
-
-# # Change ownership of all CSV files to the current user
-# sudo find "$TARGET_DIR" -type f -name "*.csv" -exec chown $(whoami):$(whoami) {} \;
-
-# # Make all CSV files writable by the owner
-# sudo find "$TARGET_DIR" -type f -name "*.csv" -exec chmod u+w {} \;
-
-# # Make all files and directories writable for the owner
-# sudo find "$TARGET_DIR" -type f -exec chmod u+w {} \;
-# sudo find "$TARGET_DIR" -type d -exec chmod u+w {} \
