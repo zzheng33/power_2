83b615b214f5fdf039fa7ea08ac30da1d0ad5a64
zzheng33 <zz695920754@gmail.com>
2025-02-24 22:05:18 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/exp_power_cap-checkpoint.py
+++ GPGPU/script/.ipynb_checkpoints/exp_power_cap-checkpoint.py
@@ -26,8 +26,8 @@ run_npb = "./run_benchmark/run_npb.py"
 
 # Define your benchmarks, for testing replace the list with just ['FT'] for example
 # ecp_benchmarks = ['FT', 'CG', 'LULESH', 'Nekbone', 'AMG2013', 'miniFE']
-ecp_benchmarks = ['XSBench','miniGAN','CRADL','sw4lite','Laghos','bert_large','UNet','Resnet50','lammps']
-ecp_benchmarks = ['CRADL','bert_large','UNet','Resnet50']
+ecp_benchmarks = ['XSBench','miniGAN','CRADL','sw4lite','Laghos','bert_large','UNet','Resnet50','lammps','gromacs']
+# ecp_benchmarks = ['CRADL','bert_large','UNet','Resnet50']
 
 # npb_benchmarks = ['bt','cg','ep','ft','is','lu','mg','sp','ua','miniFE']
 # npb_benchmarks = ['is']
